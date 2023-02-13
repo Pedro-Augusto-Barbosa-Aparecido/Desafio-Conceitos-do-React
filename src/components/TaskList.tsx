53d4be72709533170666fe269e838cf0b490472b
Pedro Augusto Barbosa Aparecido <pedro007augustobarbosa@gmail.com>
2023-02-12 21:28:01 -0300

    finish challange

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,33 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  const MAX_RANDOM_NUMBER_ID = 1000
+
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle.trim()) {
+      return
+    }
+
+    setTasks(prevState => [...prevState, {
+      id: Math.ceil(Math.random() * MAX_RANDOM_NUMBER_ID),
+      isComplete: false,
+      title: newTaskTitle.trim()
+    }])
+  
+    setNewTaskTitle("")
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(state => state.map(task => {
+      if (task.id === id)
+        task.isComplete = !task.isComplete
+
+      return task
+    }))
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(state => state.filter(task => task.id !== id))
   }
 
   return (
